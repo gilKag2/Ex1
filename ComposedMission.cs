e370527502d11dacc4785713910e4a7debef9f67
gilKag2 <35850376+gilKag2@users.noreply.github.com>
2019-03-21 18:51:23 +0200

    Base

---
+++ ComposedMission.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    public class ComposedMission : IMission
+    {
+        private List<SingleMission> missions = new List<SingleMission>();
+        private string name;
+        private string type;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+        }
+        public event EventHandler<double> OnCalculate;
+
+        public double Calculate(double value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ MissionContainer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+
+    public class FunctionsContainer
+    {
+        Dictionary<string, Delegate> container = new Dictionary<string, Delegate>();
+        public Delegate this[string func]
+        {
+            get
+            {
+                return container[func];
+            }
+            set
+            {
+                container[func] = value;
+            }
+        }
+    }
+}
